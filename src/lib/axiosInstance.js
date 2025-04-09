11c84e1db802b4521b7ae8e4ce66c27acfa61a52
Aakash1044 <aakash.a4iru5a@gmail.com>
2025-04-09 12:33:31 +0530

    feat: endpoint change

--- src/lib/axiosInstance.js
+++ src/lib/axiosInstance.js
@@ -1,11 +1,13 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api/v2",
+  // baseURL: "http://localhost:5000/api/v2",
+  baseURL:"https://ducktail-backend-temp-yyy42.ondigitalocean.app",
 });
 
 const axiosInstancev1 = axios.create({
-  baseURL: "http://localhost:5000/api/v1"
+  // baseURL: "http://localhost:5000/api/v1"
+  baseURL:"https://ducktail-backend-temp-yyy42.ondigitalocean.app",
 }) 
 
 export {axiosInstance, axiosInstancev1};

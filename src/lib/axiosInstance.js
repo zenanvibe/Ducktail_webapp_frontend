8dcbb3545b05f3a0a42e4bc76580c2f84ec8615b
Aakash1044 <aakash.a4iru5a@gmail.com>
2025-04-09 15:18:55 +0530

    feat: endpoint versions add

--- src/lib/axiosInstance.js
+++ src/lib/axiosInstance.js
@@ -2,12 +2,12 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   // baseURL: "http://localhost:5000/api/v2",
-  baseURL:"https://ducktail-backend-temp-yyy42.ondigitalocean.app",
+  baseURL:"https://ducktail-backend-temp-yyy42.ondigitalocean.app/api/v2",
 });
 
 const axiosInstancev1 = axios.create({
   // baseURL: "http://localhost:5000/api/v1"
-  baseURL:"https://ducktail-backend-temp-yyy42.ondigitalocean.app",
+  baseURL:"https://ducktail-backend-temp-yyy42.ondigitalocean.app/api/v1",
 }) 
 
 export {axiosInstance, axiosInstancev1};
